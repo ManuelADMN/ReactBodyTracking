1aa3c9322f86d760c03e81a39d25856c1c105027
ManuelADMN <ma.dzm@outlook.com>
2025-05-09 01:54:00 -0400

    Empezamos

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ARViewer from './components/ARViewer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Body Tracking AR Viewer</h1>
+      <ARViewer />
+    </div>
+  );
+}
+
+export default App;
